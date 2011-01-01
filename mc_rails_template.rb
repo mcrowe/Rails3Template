9baeea2a6fdd242fd707c884d9c2ee4919fa7a72
Mitch Crowe <crowe.mitch@gmail.com>
2011-01-01 14:42:39 -0800

    Use jquery 1.4.4 to match master version of rails.js. Remove jquery-ui by default.

--- mc_rails_template.rb
+++ mc_rails_template.rb
@@ -18,8 +18,8 @@ GENERATORS
 application generators
 
 # Get JQuery and JQuery Adapter for Rails UJS
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js",  "public/javascripts/jquery.js"
+#get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
 run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
 
 # Include JQuery and UJS adapter in javascript ":defaults"
